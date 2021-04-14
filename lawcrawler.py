48d96a0fc1f81ebfecfc0ac865689891fa6b8a1b
Suz <2ricecake@gmail.com>
2021-04-14 11:29:28 +0900

    change

--- lawcrawler.py
+++ lawcrawler.py
@@ -161,7 +161,6 @@ def collect_doc_links(query):
 
 def main():
     user_input = input('[Article number]: ')
-<<<<<<< HEAD
     article_list = user_input.split(',')
 
     for i in range(len(article_list)):
@@ -179,18 +178,6 @@ def main():
             print('No links found.')
         print("[Article {} Crawling complete.]".format(article_list[i]))
         print('--------------------------------------')
-=======
-    article_nr = '%0*d'%(4, int(user_input)) #if len(user_input) < 5, pads user_input with 0 
-    links = collect_doc_links(article_nr)
-    print('[Found {} links to download.]'.format(len(links)))
-    if links:
-        user_folder_name = '형법{}조'.format(user_input)
-        download_count = save_links_to_files(links,user_folder_name)
-        print('[{} out of {} cases downloaded.]'.format(download_count, len(links)))
-    else:
-        print('No links found.')
-    print("[Crawling complete.]")
->>>>>>> parent of 1a9baae (completed Todo3, 4)
     
 if __name__ == '__main__':
     main()
